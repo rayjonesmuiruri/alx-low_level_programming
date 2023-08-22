5b5fda46c398722f1ec06c4d5187cb98fe35aace
rayjonesmuiruri <rjmuiruri@gmail.com>
2023-08-22 03:57:44 -0700

    PRINT every character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,17 +1,29 @@
 #include "main.h"
-
 /**
- * puts_half - print the last half of a string
- * @str: string to be processed
- * Description: if the string length is odd print the last (length - 1) / 2
+ * puts2 - function should print only one character out of two
+ * starting with the first one
+ * @str: input
+ * Return: print
  */
-void puts_half(char *str)
+void puts2(char *str)
 {
-	int i, j;
+	int longi = 0;
+	int t = 0;
+	char *y = str;
+	int o;
 
-	for (i = 0; str[i] != '\0'; ++i)
-		;
-	for (j = (i + 1) / 2; j < i; ++j)
-		_putchar(str[j]);
+	while (*y != '\0')
+	{
+		y++;
+		longi++;
+	}
+	t = longi - 1;
+	for (o = 0 ; o <= t ; o++)
+	{
+		if (o % 2 == 0)
+	{
+		_putchar(str[o]);
+	}
+	}
 	_putchar('\n');
 }
