b22456b0874598cff45d467b6ef2d95d5fa0007f
rayjonesmuiruri <rjmuiruri@gmail.com>
2023-10-20 13:55:39 +0300

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
 * add_node_end - function that adds a new node
-* at the end of a list_t list
+* At the end of a list_t list
 * @head: Parameter that point to de head of the linked list
 * @str: Parameter with the string
 * Return: the address of the new element, or NULL if it failed
